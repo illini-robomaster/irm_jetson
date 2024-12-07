277194c894da37fa9a083aa0e27deaaea2ca553a
= <jerrywang197@gmail.com>
2024-12-07 17:58:38 -0600

    add uart files

---
+++ src/include/board/uart.h
@@ -0,0 +1,41 @@
+/****************************************************************************
+ *                                                                          *
+ *  Copyright (C) 2024 RoboMaster.                                          *
+ *  Illini RoboMaster @ University of Illinois at Urbana-Champaign          *
+ *                                                                          *
+ *  This program is free software: you can redistribute it and/or modify    *
+ *  it under the terms of the GNU General Public License as published by    *
+ *  the Free Software Foundation, either version 3 of the License, or       *
+ *  (at your option) any later version.                                     *
+ *                                                                          *
+ *  This program is distributed in the hope that it will be useful,         *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *  GNU General Public License for more details.                            *
+ *                                                                          *
+ *  You should have received a copy of the GNU General Public License       *
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.    *
+ *                                                                          *
+ ****************************************************************************/
+
+#include <iostream>
+#include <unistd.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <string.h>
+
+namespace SERIAL
+{
+  class UART
+  {
+  public:
+    UART(const char *name = "/dev/ttyTHS0");
+    void Transmit(uint8_t *dat, int len);
+    void Receive();
+    void Close();
+
+  private:
+    int fd;
+    struct termios options;
+  };
+}
