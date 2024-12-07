58696ae70b459e08fd8b11eb8e028e7eee9960fc
Ziyi Wang <anderprima54@gmail.com>
2024-12-07 16:54:53 -0600

    added uart and some can perif

--- src/shared/lib/uart.cc
+++ src/shared/lib/uart.cc
@@ -17,51 +17,34 @@
  *  along with this program. If not, see <http://www.gnu.org/licenses/>.    *
  *                                                                          *
  ****************************************************************************/
-#include "can.h"
+#include <iostream>
+#include <wiringSerial.h>
 
-namespace CANRAW {
-
-CAN::CAN(const char* name) {
-    s = socket(PF_CAN, SOCK_RAW, CAN_RAW);
-    if (s < 1) {
-        std::cerr << "Error while opening socket" << std::endl;
-    }
-
-    strcpy(ifr.ifr_name, name);
-    ioctl(s, SIOCGIFINDEX, &ifr);
-
-    addr.can_family = AF_CAN;
-    addr.can_ifindex = ifr.ifr_ifindex;
-
-    if (bind(s, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
-        std::cerr << "Error while binding address" << std::endl;
-    }
-}
-
-void CAN::Transmit(canid_t can_id, uint8_t *dat, int len) {
-    ftx.can_id = can_id;
-    ftx.can_dlc = clamp(len, 0, CAN_MAX_DLEN);
-    memcpy(ftx.data, dat, sizeof(uint8_t) * ftx.can_dlc);
-    if (write(s, &ftx, sizeof(struct can_frame)) !=
-            sizeof(struct can_frame)) {
-        std::cerr << "Error while sending CAN frame" << std::endl;
-    }
-}
-
-void CAN::Receive() {
-    if (read(s, &frx, sizeof(struct can_frame)) !=
-            sizeof(struct can_frame)) {
-        std::cerr << "Error while receiving CAN frame" << std::endl;
-    }
-}
-
-int RegisterRxCallback(canid_t can_id, can_rx_callback_t callback, void *args)
+int main()
 {
-    return 0;
-}
+  int fd;
 
-void CAN::Close() {
-    close(s);
-}
+  // Open the serial port
+  fd = serialOpen("/dev/ttyACM0", 115200); // Adjust the port and baud rate as needed
 
+  if (fd < 0)
+  {
+    std::cerr << "Unable to open serial port" << std::endl;
+    return 1;
+  }
+
+  // Write data to the serial port
+  serialPuts(fd, "Hello from Jetson Orin Nano!\n");
+
+  // Read data from the serial port
+  while (serialDataAvail(fd))
+  {
+    char c = serialGetchar(fd);
+    std::cout << c;
+  }
+
+  // Close the serial port
+  serialClose(fd);
+
+  return 0;
 }
